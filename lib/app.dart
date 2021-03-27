87d7f547e995209e2e1321c1b42f30c0c4a191a9
akambi <akambi.sylv@gmail.com>
2021-03-27 20:53:02 +0100

    feat: modif

--- lib/app.dart
+++ lib/app.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:provider/provider.dart';
-import 'package:version_banner/version_banner.dart';
-import 'package:flavor/flavor.dart' as fl;
 
 class VIPAPP extends StatelessWidget {
   @override
@@ -21,30 +19,24 @@ class VIPAPP extends StatelessWidget {
       ],
       child: Consumer<Auth>(
         builder: (ctx, auth, _) => LocalizationProvider(
-            state: LocalizationProvider.of(context).state,
-            child: VersionBanner(
-              color: Colors.red,
-              visible: fl.Flavor.I.isDevelopment ? true : false,
-              location: BannerLocation.bottomEnd,
-              text: "DEV",
-              textStyle: TextStyle(),
-              child: MaterialApp(
-                  localizationsDelegates: [
-                    GlobalMaterialLocalizations.delegate,
-                    GlobalWidgetsLocalizations.delegate,
-                    localizationDelegate
-                  ],
-                  supportedLocales: localizationDelegate.supportedLocales,
-                  locale: localizationDelegate.currentLocale,
-                  theme: ThemeData(
-                      fontFamily: 'Poppins',
-                      primaryColor: ColorsData.colorsPrimary),
-                  title: ExtraString.appName,
-                  debugShowCheckedModeBanner: false,
-                  initialRoute: Routes.StartUpView,
-                  home: StartUpView(),
-                  onGenerateRoute: RouteGenerator.generateRoute),
-            )),
+          state: LocalizationProvider.of(context).state,
+          child: MaterialApp(
+              localizationsDelegates: [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                localizationDelegate
+              ],
+              supportedLocales: localizationDelegate.supportedLocales,
+              locale: localizationDelegate.currentLocale,
+              theme: ThemeData(
+                  fontFamily: 'Poppins',
+                  primaryColor: ColorsData.colorsPrimary),
+              title: ExtraString.appName,
+              debugShowCheckedModeBanner: false,
+              initialRoute: Routes.StartUpView,
+              home: StartUpView(),
+              onGenerateRoute: RouteGenerator.generateRoute),
+        ),
       ),
     );
   }
